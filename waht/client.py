a0617428d0466f9ad3664b82349eb1315ff480da
meiski <meiskiv@gmail.com>
2016-12-06 13:06:14 -0200

    [to be continued]

--- waht/client.py
+++ waht/client.py
@@ -1,13 +1,37 @@
+import time
+import threading
 import socket
+import json
 
-HOST = '10.0.2.15'     # Endereco IP do Servidor
 PORT = 54321            # Porta que o Servidor esta
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-dest = (HOST, PORT)
-tcp.connect(dest)
-print 'Para sair use CTRL+X\n'
-msg = raw_input()
-while msg != '\x18':
-    tcp.send(msg)
-    msg = raw_input()
-tcp.close()
+
+with open('/home/meiski/PycharmProjects/P2P/pcp/files/ips.txt','r') as f:
+    ips = f.read()
+ips = ips.splitlines()
+threads = []
+
+
+class Client(threading.Thread):
+    def __init__(self,ip):
+        threading.Thread.__init__(self)
+        self.ip = ip
+    def run(self):
+        print 'Fazendo requisicao no IP: ', self.ip
+        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        dest = (self.ip, PORT)
+        tcp.connect(dest)
+        json_string = '{"tipo": "pli", "dados": "None"}' #aqui os dados precisam ser null ou uma string null>como fazer??
+        jserial = json.dumps(json_string)
+        tcp.send(jserial)
+        tcp.close()
+
+
+print 'iniciando requisicoes'
+for t in range(len(ips)):
+    thread = Client(ips[t])
+    thread.start()
+    threads.append(thread)
+    time.sleep(3.5)
+
+for t in threads:
+    t.join()
--- waht/thread_concrr.py
+++ waht/thread_concrr.py
@@ -1,31 +1,44 @@
 import socket
+import json
 import thread
 
-#servidor vou ter que passar por parametro
-HOST = '192.168.1.6'              # Endereco IP do Servidor
-PORT = 54321            # Porta que o Servidor esta
+HOST = '192.168.25.27'              # Endereco IP do Servidor
+PORT = 54321                        #  Porta que o Servidor esta
 
-def conectado(con, cliente):
-    print 'Conectado por', cliente
 
-    while True:
-        msg = con.recv(1024)
-        if not msg: break
-        print cliente, msg
+#json_dados = '{"tipo": tipo, "dados": dados'}'
+def send_rar(dados):
+    with open ('/home/meiski/PycharmProjects/P2P/pcp/files/arquivos.txt') as f:
+        arqs = f.readline()
+        if arqs == dados:
+            #lógica pra mandar nome do arquivo e arquivo codificado blah blah
 
-    print 'Finalizando conexao do cliente', cliente
-    con.close()
-    thread.exit()
-
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-orig = (HOST, PORT)
-
-tcp.bind(orig)
-tcp.listen(1)
 
 while True:
-    con, cliente = tcp.accept()
-    thread.start_new_thread(conectado, tuple([con, cliente]))
+    def conectado(con, cliente):
+        print 'Conectado por', cliente
 
-tcp.close()
+        while True:
+            quadro = con.recv(j)
+            deserj = json.loads(quadro)
+            print deserj
+            print cliente, quadro
+
+            if (deserj['tipo']) == 'par':
+                send_rar(quadro.dados)
+            print 'Finalizando conexao do cliente', cliente
+            con.close()
+            thread.exit()
+
+    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    orig = (HOST, PORT)
+    tcp.bind(orig)
+    tcp.listen(1)
+
+    while True:
+        con, cliente = tcp.accept()
+        thread.start_new_thread(conectado, tuple([con, cliente]))
+
+    tcp.close()
+
+
